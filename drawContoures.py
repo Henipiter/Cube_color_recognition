803c84b55e403f2f1b5d82a5da3d3730af0a909c
Henipiter <henipiter@wp.pl>
2020-11-19 00:11:34 +0100

    f

--- drawContoures.py
+++ drawContoures.py
@@ -38,15 +38,12 @@ def makeMark(cnts, ori_image, image, areas, showParam):
 		area = cv2.contourArea(c)
 		if(M["m00"]!=0): #and area== max(areas)): #area > max(areas)//20 ):
 			avgColor = getAverageColor(ori_image, image, c)
-		
 			nameC = colorIdentify.colorName(avgColor)
 			nameC = nameC+str(i)
 			i+=1
 			cX = int(M["m10"] / M["m00"])
 			cY = int(M["m01"] / M["m00"])
-			
-			
-			cv2.drawContours(ori_image, [c], -1, (160, 255, 255), 10)
+			#cv2.drawContours(ori_image, [c], -1, (160, 255, 255), 10)
 			cv2.circle(ori_image, (cX, cY), 12, (0, 255, 255), -1)
 			cv2.putText(ori_image, nameC, (cX - 20, cY - 20),
 			cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 10)
--- main.py
+++ main.py
@@ -11,11 +11,11 @@ directorySource = "./source/"
 directoryResult = "./result/"
 nameList=[]
 singlePhoto = "white4.jpg"
-loop = False
-showParam = True
+loop = True
+showParam = False
 if( loop ):
-	for i in range (1,8):
-		nameList.append("img"+str(i)+".jpg")
+	for i in range (1,4):
+		nameList.append("white"+str(i)+".jpg")
 else:
 	nameList.append(singlePhoto)
 
@@ -35,32 +35,28 @@ for name in nameList:
 	## usuniecie tla z obrazu
 	
 	cube_img = filter.eraseBackground(ori_image, contourCube)
-	cv2.imwrite(directoryResult+"F3i2inal_"+name, cube_img)
 	#######################
 	#cv2.imwrite(directoryResult+"Fiinal_"+name, img)
 	#image = cv2.imread(directoryResult+"Fiinal_"+name)
 
 	dark_image = filter.darkFilter(cube_img)
-	cv2.imwrite(directoryResult+"dark.jpg", dark_image)
 	
 	cnts = drawContoures.findContour(dark_image)
 	areas = drawContoures.findAreas(cnts, showParam)
 	
 	original = cv2.imread(directorySource+name)
-	original = filter.increase_sharpness(original, 80)
+	#original = filter.increase_sharpness(original, 80)
 	#original = filter.adjust_gamma(original, gamma=1.5)
-	#ori_image = filter.adjust_gamma(ori_image, gamma=1.5)
 			
-	original = cv2.cvtColor(ori_image, cv2.COLOR_BGR2HSV)
+	original = cv2.cvtColor(original, cv2.COLOR_BGR2HSV)
 	original = drawContoures.makeMark(cnts, original, dark_image, areas, showParam)
-	
 	original = cv2.cvtColor(original, cv2.COLOR_HSV2BGR)
 	#ori_image = filter.adjust_gamma(ori_image, gamma=1.5)
 	#ori_image = cv2.cvtColor(ori_image, cv2.COLOR_BGR2HSV)
 	#ori_image = drawContoures.makeMark([c], ori_image, image, areas,showParam)
 	#ori_image = cv2.cvtColor(ori_image, cv2.COLOR_HSV2BGR)
 	
-	cv2.imwrite(directoryResult+"original.jpg", original)
+	cv2.imwrite(directoryResult+"Final_"+name, original)
 	
 	
 	print("Processing done.\n")
